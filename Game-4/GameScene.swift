a6363adf198b8e4be66f394d78016578a02c18f0
Subhrajyoti <subha.ionic3@gmail.com>
2020-08-19 16:42:25 +0530

    initial code base

---
+++ Game-4/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Game-4
+//
+//  Created by Subhrajyoti Chakraborty on 19/08/20.
+//  Copyright © 2020 Subhrajyoti Chakraborty. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+
+}
